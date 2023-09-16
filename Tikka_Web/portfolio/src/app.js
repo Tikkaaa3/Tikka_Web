07d4ef806813715df8f1219755308edbc41d01c8
Tikkaaa3 <tikkaaa3@gmail.com>
2023-09-17 01:04:57 +0200

    foundation

--- Tikka_Web/portfolio/src/app.js
+++ Tikka_Web/portfolio/src/app.js
@@ -7,23 +7,6 @@ export default function main ()  {
   
   const mainHTML = `
   <div>
-  <div id="about" class="flex flex-row justify-between items-center">
-    <div class="pl-48">
-      <h1 class="text-7xl font-bold my-8">Hello, I am</h1>
-      <h1 class="text-7xl text-white font-bold my-8">Software Developer</h1>
-      <p class="text-custom-white">Talks about #python, #webdeveloper, #cloudcomputing, #softwaredeveloper, and #fullstackdeveloper</p>
-      <button id="custom-button">Show CV</button>
-      <button id="custom-button">Download CV</button>
-    </div>
-  
-    <div class="mx-2 pr-48">
-      <div class="w-96 h-96 rounded-full overflow-hidden">
-        <img src="/javascript.svg" alt="JavaScript Logo" class="object-cover w-full h-full" />
-      </div>
-    </div>
-  </div>
-  <!-- Space -->
-<div class="py-20"></div>
   </div>
   `;
   
